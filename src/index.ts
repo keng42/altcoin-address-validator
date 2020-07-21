35904bcb8cd9300662061e33d6ed75122ce1b204
keng42 <me@keng42.com>
2020-07-21 15:57:27 +0800

    feat: add chain Tron

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Ethereum } from './ethereum';
+import { Tron } from './tron';
 
 export * from './ethereum';
+export * from './tron';
 
 const chain2class = {
   ethereum: Ethereum,
   eth: Ethereum,
+  tron: Tron,
+  trx: Tron,
 };
 
---
+++ src/tron.ts
@@ -0,0 +1,62 @@
+import jsSHA from 'jssha';
+import { decodeBase58, byteArray2hexStr, hexStr2byteArray } from './utils';
+
+export class Tron {
+  static isValidAddress(
+    address: string,
+    network: 'main' | 'testnet' = 'main'
+  ): boolean {
+    const addressBytes = this.decodeBase58Address(address);
+
+    if (!addressBytes) {
+      return false;
+    }
+
+    if (addressBytes.length !== 21) {
+      return false;
+    }
+
+    if (network === 'main') {
+      return addressBytes[0] === 0x41;
+    }
+    return addressBytes[0] === 0xa0;
+  }
+
+  static decodeBase58Address(base58Address: string): number[] | null {
+    if (base58Address.length <= 4) {
+      return null;
+    }
+
+    let address: number[];
+    try {
+      address = decodeBase58(base58Address);
+    } catch (e) {
+      return null;
+    }
+
+    const len = address.length;
+    const offset = len - 4;
+    const checkSum = address.slice(offset);
+    address = address.slice(0, offset);
+
+    let shaObj = new jsSHA('SHA-256', 'HEX');
+    shaObj.update(byteArray2hexStr(address));
+    const hash0 = shaObj.getHash('HEX');
+
+    shaObj = new jsSHA('SHA-256', 'HEX');
+    shaObj.update(hash0);
+    const hash1 = hexStr2byteArray(shaObj.getHash('HEX'));
+
+    const checkSum1 = hash1.slice(0, 4);
+    if (
+      checkSum[0] === checkSum1[0] &&
+      checkSum[1] === checkSum1[1] &&
+      checkSum[2] === checkSum1[2] &&
+      checkSum[3] === checkSum1[3]
+    ) {
+      return address;
+    }
+
+    return null;
+  }
+}
